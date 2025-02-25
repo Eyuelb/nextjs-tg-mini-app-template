bf73d8e9430486660d057f62e037d10e9afdd0e5
Eyuel Belete <eyuelbelete98@gmail.com>
2025-02-26 01:35:09 +0300

    first commit

---
+++ src/core/i18n/locale.ts
@@ -0,0 +1,24 @@
+//use server is required
+"use server";
+
+import { cookies } from "next/headers";
+
+import { defaultLocale } from "./config";
+import type { Locale } from "./types";
+
+// In this example the locale is read from a cookie. You could alternatively
+// also read it from a database, backend service, or any other source.
+const COOKIE_NAME = "NEXT_LOCALE";
+
+const getLocale = async () => {
+  const { get } = await cookies();
+
+  return get(COOKIE_NAME)?.value || defaultLocale;
+};
+
+const setLocale = async (locale?: string) => {
+  const { set } = await cookies();
+  set(COOKIE_NAME, (locale as Locale) || defaultLocale);
+};
+
+export { getLocale, setLocale };
