dcc8cc34e38c79cf8c3f0177795e1ea56d95dd4a
Eyuel Belete <eyuelbelete98@gmail.com>
2025-02-26 02:49:44 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import { useEffect } from "react";
 export default function Home() {
-  useEffect(() => {
-    import("eruda").then((lib) => lib.default.init()).catch(console.error);
-
-    return () => {};
-  }, []);
+  import("eruda").then((lib) => lib.default.init()).catch(console.error);
 
   return (
     <div>
