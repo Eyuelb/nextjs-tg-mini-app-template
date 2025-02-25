828cf19504b9dd14f38ae91b9b857221d1bb2ea0
Eyuel Belete <eyuelbelete98@gmail.com>
2025-02-26 01:38:15 +0300

    feat: added standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
-import createNextIntlPlugin from 'next-intl/plugin';
- 
+import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  compiler: {
+    removeConsole: true,
+  },
+};
 
 export default withNextIntl(nextConfig);
