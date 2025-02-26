32ccb8a9491ec5c9b295b9aedc107b1672307c7c
Eyuel Belete <eyuelbelete98@gmail.com>
2025-02-26 03:19:26 +0300

    fix

--- src/providers/app.providers.tsx
+++ src/providers/app.providers.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { PropsWithChildren } from "react";
-import RootStyleRegistry from "./mantine";
 import { I18nProvider } from "@/core/i18n/provider";
 import { Root } from "@/components/Root/Root";
 import { useDidMount } from "@/hooks/useDidMount";
+import dynamic from "next/dynamic";
+const RootStyleRegistry = dynamic(() => import("./mantine"), { ssr: false });
 
 function GlobalProviders({ children }: PropsWithChildren) {
   const didMount = useDidMount();
 
   if (!didMount) {
-    return 'lading';
-
+    return "lading";
   }
   return <RootStyleRegistry>{children} </RootStyleRegistry>;
 }
